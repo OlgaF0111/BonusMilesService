0b7a5d4509651a2b99e48901f0efc5ba99341cd1
Olga F <krasota1_00@mail.ru>
2023-02-13 20:07:26 +0700

    README comment

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,6 @@
 public class BonusMilesService {
-    public int calculate(int cost) {
+    public int calculate(int cost) {  // не понимаю смысла int cost?
+        // почему нельзя было в параметрах перечислить price и bonus?
         int price = 10_000;
         int bonus = 20;
         int miles = (price / bonus);
